352140a760051115ddce99b9480a2e0f872fcf15
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-24 08:53:02 -0500

    Update CoolRune-Files-Install.sh

--- Desktop/CoolRune-Files-Install.sh
+++ Desktop/CoolRune-Files-Install.sh
@@ -4,7 +4,7 @@ touch /home/$USER/Desktop/CoolRune-Files
 sudo mv /home/$USER/Desktop/CoolRune-Files/bin /
 sudo mv /home/$USER/Desktop/CoolRune-Files/-CoolRune- /
 sudo mv /home/$USER/Desktop/CoolRune-Files/etc /
-sudo mv /home/$USER/Desktop/CoolRune-Files/usr
+sudo mv /home/$USER/Desktop/CoolRune-Files/usr /
 sudo rm /home/$USER/Desktop/CoolRune-Files/CoolRune-Files
 sudo update-grub 
 s6-db-reload
