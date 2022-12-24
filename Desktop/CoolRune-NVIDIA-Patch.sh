5e73f86adc174ec4abdfa0b8d1b35dd9818a24db
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2022-12-24 14:17:34 -0500

    Update CoolRune-NVIDIA-Patch.sh

--- Desktop/CoolRune-NVIDIA-Patch.sh
+++ Desktop/CoolRune-NVIDIA-Patch.sh
@@ -4,3 +4,4 @@ touch /home/$USER/Desktop/CoolRune-NVIDIA-Patch
 sudo mv /home/$USER/Desktop/CoolRune-NVIDIA-Patch/etc /
 sudo rm /home/$USER/Desktop/CoolRune-NVIDIA-Patch
 sudo update-grub 
+sudo reboot
